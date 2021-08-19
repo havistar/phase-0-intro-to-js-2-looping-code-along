70f013dd78d6c57721786e90161977046b1548d1
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-18 22:05:52 -0500

    commit tree 9fe273266369d44237a3461b71a346c851eb7305

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Code your solutions in this file
 
-const name = ["Lisa", "Kaitlin", "Jan"];
-const surprise = "surprise";
+const name = ['Lisa', 'Kaitlin', 'Jan'];
+const surprise = 'surprise';
 
 function writeCards(name, surprise) {
-    let messageArray =[];
+  let messageArray = [];
   for (let i = 0; i < name.length; i++) {
     let message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift!`
     messageArray.push(message);
   }
 
-  return (messageArray);
+
+return(messageArray);
 }
 
-
-
 function countDown(int){
-    let i = 10;
-    while (i>=0) {
-        console.log (i);
-        i--
-    }
-    return int;
+  let i = 10;
+  while (i>=0) {
+    console.log (i);
+    i--
+
+  }
+  return int;
 }
